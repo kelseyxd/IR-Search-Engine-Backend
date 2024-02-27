259e818771462701a38fd8d993da1f4978b212be
yrhyrhyrh <yu.runhan12@gmail.com>
2024-02-28 00:13:22 +0800

    Implement insertion of multiple documents as a list

--- src/main/java/com/example/springbootelasticsearchexample/repo/ProductRepo.java
+++ src/main/java/com/example/springbootelasticsearchexample/repo/ProductRepo.java
@@ -3,7 +3,7 @@ package com.example.springbootelasticsearchexample.repo;
 import com.example.springbootelasticsearchexample.entity.Product;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-// The arguments are the entity(Product) and the type of id (integer)
-public interface ProductRepo extends ElasticsearchRepository<Product,Integer>{ // by extendind the ElasticsearchRepository, ProductRepo inherits various methods for performing CRUD on Elasticsearch documents
+// Params accepts a product of type Product, and ID of product of type Integer
+public interface ProductRepo extends ElasticsearchRepository<Product,Integer> { // by extendind the ElasticsearchRepository, ProductRepo inherits various methods for performing CRUD on Elasticsearch documents
 
 }
